85bf47d0438db603c106052b49cfd5281cec64d2
coronado17 <36279731+coronado17@users.noreply.github.com>
2018-02-27 20:48:58 -0600

    Update project3code.py
    Created dictionaries and counter variables to hold data

--- project3code.py
+++ project3code.py
@@ -5,3 +5,15 @@ url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
 response = urlopen(url)
 
 logfile = response.read().decode('utf-8').split('\n')
+
+#will have count by day for week/month
+count_dict = {}
+4xx_error = 0
+3xx_error = 0
+# count for unique request file
+request_file = {}
+# saves last actual month
+lastmonth = 0
+# file variable (for open and write data to file) that allows to write data by month
+currentfile = None
+total = 0
